75eb9a2559d4964142e68e39f8ea956850d9ec54
Daven Young <davenyoung@msn.com>
2022-04-11 17:28:42 -0600

    commit tree fc9d6666b1a8b44815ee8735e9547e693c8ce67b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () =>{
+  const newText = document.getElementById('text');
+  document.getElementById('text').innerHTML = 'This is really cool!'
+  });
+  
+
+const newText = document.getElementById('text');
+console.log(newText);
